a822b77fad7ba16486dc8467832b642f62a22d75
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-02-19 12:28:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let namebox = document.querySelector('input')
+namebox.addEventListener('click', ()=>{
+	let uppercase = namebox.value.toUpperCase()
+	namebox.value = uppercase
+})
